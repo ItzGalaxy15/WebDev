cc29173453df744d62c2220e5d276cfb6cbf6e74
Aymane <152269683+ItzGalaxy15@users.noreply.github.com>
2024-09-18 13:25:35 +0200

    Implemented CheckPassword
    Checks for correct password or username to log the user in

--- Services/LoginService.cs
+++ Services/LoginService.cs
@@ -20,6 +20,17 @@ public class LoginService : ILoginService
     public LoginStatus CheckPassword(string username, string inputPassword)
     {
         // TODO: Make this method check the password with what is in the database
+
+        var admin = _context.Admin.FirstOrDefault(a => a.UserName == username);
+        if (admin == null)
+        {
+            return LoginStatus.IncorrectUsername;
+        }
+        if (admin.Password == EncryptionHelper.EncryptPassword(inputPassword))
+        {
+            return LoginStatus.Success;
+        }
+
         return LoginStatus.IncorrectPassword;
     }
 }
