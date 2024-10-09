1d66c215858e7bea9974e6795ac61a5c75d6ba46
Jordy <jordym_1999@hotmail.com>
2024-10-09 13:12:18 +0200

    Updates messages to remove double code

--- Services/IMessageService.cs
+++ Services/IMessageService.cs
@@ -4,7 +4,7 @@ namespace StarterKit.Services;
 
 public interface IMessageService {
     public Task<List<Message>> GetMessageById(int id);
-    public Task<(bool,int)> CheckLogin(string? session);
+    //public Task<(bool,int)> CheckLogin(string? session);
     public Task CreateMessage(Message mes, int To_id, int Current_id);
     public Task<bool> MessageRead(int uid, int mId);
 }
