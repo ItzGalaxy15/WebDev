940f5c3e4474ed37444e8f0e6568b8327c61be98
ItzGalaxy15 <152269683+ItzGalaxy15@users.noreply.github.com>
2024-12-11 13:41:52 +0100

    Added login page

--- client-side/Home/Home.tsx
+++ client-side/Home/Home.tsx
@@ -1,58 +1,57 @@
 import React from "react";
-import { HomeState, initHomeState, Person } from './home.state';
+import { HomeState, initHomeState } from './home.state';
 import { RegistrationForm } from "../registration/registration";
-import { OverviewPage } from "../Overview/overview"
+import { OverviewPage } from "../Overview/overview";
+import Login from "../Login/login"; // Import the Login component
 
-export class Home extends React.Component<{},HomeState>
-{
-    constructor(props: {}){
-        super(props)
-        this.state = initHomeState
+export class Home extends React.Component<{}, HomeState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = initHomeState;
+  }
+
+  render(): JSX.Element {
+    if (this.state.view === "home") {
+      return (
+        <div>
+          Welcome to our home page
+          <div>
+            <button
+              onClick={() => this.setState(this.state.updateViewState("registration"))}
+            >
+              Registration
+            </button>
+            <button
+              onClick={() => this.setState(this.state.updateViewState("overview"))}
+            >
+              Overview
+            </button>
+            <button
+              onClick={() => this.setState(this.state.updateViewState("login"))}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      );
+    } else if (this.state.view === "registration") {
+      return (
+        <RegistrationForm
+          backToHome={() => this.setState(this.state.updateViewState("home"))}
+        />
+      );
+    } else if (this.state.view === "login") {
+      return (
+        <Login
+          backToHome={() => this.setState(this.state.updateViewState("home"))}
+        />
+      );
+    } else {
+      return (
+        <OverviewPage
+          backToHome={() => this.setState(this.state.updateViewState("home"))}
+        />
+      );
     }
-    
-    render():JSX.Element{
-        
-        if (this.state.view === "home"){
-            return(
-                <div>
-                    Welcome to y home page
-                    <div>
-                        <button
-                            onClick={e=>this.setState(this.state.updateViewState("registration"))}
-                        >
-                            Registration</button>
-                        <button
-                            onClick={e=>this.setState(this.state.updateViewState("overview"))}
-                        >
-                            Overview</button>
-                    </div>
-                </div>
-
-
-            )
-        }
-        else if (this.state.view == "registration")
-        { 
-            return (<RegistrationForm
-                backToHome = {()=>this.setState((this.state.updateViewState("home")))}
-                // insertPerson={(person: Person)=>this.setState(this.state.insertPerson(person))}
-                // testVal = {this.state.test}
-                // testName = {this.state.testName}
-                // updateTest={(num:number) => this.state.updateTest(444)}
-                // updateTest={(num:number) => this.setState(this.state.updateTest(555))}
-                // resetFields={()=>this.setState(this.state.resetFields())}
-            />)
-        }
-        else
-        {
-            return (<OverviewPage
-                // storageToShow={this.state.storage}
-                backToHome={()=>this.setState(this.state.updateViewState("home"))}
-                
-            />
-            )
-        }
-        
-    }
-
+  }
 }
--- client-side/Home/home.state.ts
+++ client-side/Home/home.state.ts
@@ -1,61 +1,22 @@
-export type ViewState = "home" | "registration" | "overview"
+export type ViewState = "home" | "registration" | "overview" | "login";
 
 export interface Person {
-    name: string,
-    lastname: string,
-    age: number
+  name: string;
+  lastname: string;
+  age: number;
 }
 
-export interface HomeState 
-{
-    // storage: Map<number, Person>,
-    id: number,
-    view: ViewState,
-    // test: number,
-    // testName: string,
-    updateViewState: (view : ViewState) => (state: HomeState) => HomeState
-    // insertPerson: (person : Person) => (state: HomeState) => HomeState
-    // updateTest : (num: number) => (state: HomeState) => HomeState
+export interface HomeState {
+  view: ViewState;
+  updateViewState: (view: ViewState) => (state: HomeState) => HomeState;
 }
 
-
-
-export const initHomeState: HomeState = 
-{
-    view: "home",
-    // storage: new Map<number, Person>(),
-    // test: 999,
-    id: 0,
-    // testName: "Bob",
-
-
-    updateViewState: (view: ViewState) => (state: HomeState): HomeState => {
-        return {
-            ...state,
-            view: view,
-        }
-    },
-
-    // insertPerson: (person: Person) => (state: HomeState): HomeState => {
-    //     return {
-    //         ...state,
-    //         id: state.id+1,
-    //         storage: state.storage.set(state.id, {
-    //             name: person.name, 
-    //             lastname: person.lastname, 
-    //             age: person.age})
-                
-    //     }
-    // },
-
-    // updateTest : (num: number) => (state: HomeState) : HomeState => {
-    //     return {
-    //         ...state,
-    //         test: num
-    //     };
-    // },
-
-
-
-
-}
+export const initHomeState: HomeState = {
+  view: "home",
+  updateViewState: (view: ViewState) => (state: HomeState): HomeState => {
+    return {
+      ...state,
+      view: view,
+    };
+  },
+};
