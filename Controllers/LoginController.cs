d208bef4388385028dfe3729966a6a2a6c868217
Amer Alhasoun <ameralhasoun@gmail.com>
2025-01-08 15:42:39 +0100

    "update the login Controller so it respond with correct json"

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -50,16 +50,16 @@ public class LoginController : Controller
     {
         if (registerBody.Username is null || registerBody.Password is null || registerBody.FirstName is null || registerBody.LastName is null || registerBody.RecuringDays is null)  
         {
-            return BadRequest("All fields are required");
+            return BadRequest(new { message = "All fields are required"});
         }
 
         var registrationResult = await _loginService.RegisterUser(registerBody.Username, registerBody.Password, registerBody.FirstName, registerBody.LastName, registerBody.RecuringDays);
 
         return registrationResult switch
         {
-            RegistrationStatus.Success => Ok("User registered successfully"),
-            RegistrationStatus.EmailAlreadyExists => Conflict("Email already exists"),
-            _ => BadRequest("Registration failed")
+            RegistrationStatus.Success => Ok(new { message = "User registered successfully" }),
+            RegistrationStatus.EmailAlreadyExists => Conflict(new { message = "Email already exists" }),
+            _ => BadRequest(new { message = "Registration failed"})
         };
     }
 
