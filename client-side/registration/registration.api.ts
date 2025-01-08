5c1715fd23e065357f9dff4487e93717b430858d
Amer Alhasoun <ameralhasoun@gmail.com>
2025-01-08 16:38:16 +0100

    update submit() in registration.api.ts

--- client-side/registration/registration.api.ts
+++ client-side/registration/registration.api.ts
@@ -1,11 +1,30 @@
-import { Person } from "../Home/home.state";
+import { Person } from "./registration.state";
 
 export const submit = async (person: Person) : Promise<void> => {
-    await fetch("api/storage", {
-        method: "POST",
-        body: JSON.stringify(person),
-        headers: {
-            "content-type" : "application/json"
+    try{
+        const response = await fetch("api/v1/Login/Register", {
+            method: "POST",
+            body: JSON.stringify(person),
+            headers: {
+                "content-type" : "application/json"
+            }
+        });
+        if (response.ok) {
+            // const result = await response.json();
+            onSuccess(`User with name ${person.firstname} registered successfully. \nNow you can login`);
+        } else {
+            const textResult = await response.text();
+            onFailure(textResult);
         }
-    });
+    }catch (error: any) {
+        onFailure(error.errorMessage);
+    }
+}
+
+const onSuccess = (Message: string) => {
+    alert(Message);
+}
+
+function onFailure(errorMessage: string) {
+    alert("Failed to register: " + errorMessage + " \nTry again");
 }
