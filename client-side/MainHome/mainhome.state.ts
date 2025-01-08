b19536ba20475174fe404de83d26957fa0ca799b
Amer Alhasoun <ameralhasoun@gmail.com>
2025-01-08 16:36:11 +0100

    "add isRegisterd property for MainHomeState "

--- client-side/MainHome/mainhome.state.ts
+++ client-side/MainHome/mainhome.state.ts
@@ -2,12 +2,14 @@ export type ViewState = "registration" | "mainHome" | "login"
 
 
 export interface MainHomeState {
+  isRegisterd : boolean
   view: ViewState;
   isAdmin?: boolean;
   updateViewState: (view: ViewState) => (state: MainHomeState) => MainHomeState;
 }
 
 export const initMainHomeState: MainHomeState = {
+  isRegisterd: false,
   view: "mainHome",
   updateViewState: (view: ViewState) => (state: MainHomeState): MainHomeState => {
     return {
